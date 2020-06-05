f71952be7a7937566a07c92b1d21846750ed2e37
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-04 20:03:07 -0500

    Start bst traversal

---
+++ python/bst_traversal.py
@@ -0,0 +1,13 @@
+def inOrderTraverse(tree, array):
+    # Write your code here.
+    pass
+
+
+def preOrderTraverse(tree, array):
+    # Write your code here.
+    pass
+
+
+def postOrderTraverse(tree, array):
+    # Write your code here.
+    pass
--- python/validate_bst.py
+++ python/validate_bst.py
@@ -1,5 +1,5 @@
 
-# Recursive solution
+# O(n) Time | O(d) Space where d is the depth of the tree
 
 class BST:
 	def __init__(self, value):
@@ -7,13 +7,13 @@ class BST:
 		self.left = None
 		self.right = None
 
-def validateBst(tree, rootVal = None):
+def validateBst(tree):
 	return validateBstHelper(tree, float("-inf"), float("inf"))
 
-def validateBstHelper(tree, minValue, maxValue):
+def validateBstHelper(tree, minVal, maxVal):
 	if tree is None:
 		return True
-	if tree.value < minValue or tree.value >= maxValue:
+	if tree.value < minVal or tree.value >= maxVal:
 		return False
-	leftIsValid = validateBstHelper(tree.left, minValue, tree.value)
-	return leftIsValid and validateBstHelper(tree.right, tree.value, maxValue)
+	leftTree = validateBstHelper(tree.left, minVal, tree.value)
+	return leftTree and validateBstHelper(tree.right, tree.value, maxVal)
