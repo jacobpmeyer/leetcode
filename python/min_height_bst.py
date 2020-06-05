1e8f0d4fe1855e90d6b3782e332cf107c6a833d6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-05 14:16:10 -0500

    Finish min height bst

--- python/min_height_bst.py
+++ python/min_height_bst.py
@@ -1,21 +1,29 @@
-def minHeightBst(array):
-    pass
 
+# O(n) Time | O(log(n)) Space
+
+def minHeightBst(array):
+	mid = int(len(array) / 2)
+	root = BST(array[mid])
+	if len(array) > 1:
+		root.left = minHeightBst(array[:mid])
+	if len(array) > 2:
+		root.right = minHeightBst(array[mid + 1:])
+	return root
 
 class BST:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
 
-    def insert(self, value):
-        if value < self.value:
-            if self.left is None:
-                self.left = BST(value)
-            else:
-                self.left.insert(value)
-        else:
-            if self.right is None:
-                self.right = BST(value)
-            else:
-                self.right.insert(value)
+	def insert(self, value):
+		if value < self.value:
+			if self.left is None:
+				self.left = BST(value)
+			else:
+				self.left.insert(value)
+		else:
+			if self.right is None:
+				self.right = BST(value)
+			else:
+				self.right.insert(value)
