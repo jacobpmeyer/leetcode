9f2641bad5201988f3ee8edb9d767f2ef3d247da
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-10 17:11:13 -0500

    Finish single cycle check

--- python/single_cycle_check.py
+++ python/single_cycle_check.py
@@ -1,2 +1,17 @@
+
+# O(n) Time where n is the length of the array
+# O(1) Space because of constant operations
 def hasSingleCycle(array):
-	
+	numOfJumps = 0
+	index = 0
+	while numOfJumps < len(array):
+		if numOfJumps > 0 and index == 0:
+			return False
+		numOfJumps += 1
+		index = getIndex(index, array)
+	return index == 0
+
+def getIndex(index, array):
+	nextIdx = array[index]
+	nextIdx = (index + nextIdx) % len(array)
+	return nextIdx
