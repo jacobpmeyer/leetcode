6d2123d96c08c74eda57dd70964ecb48093f1e67
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-12 11:22:36 -0500

    Update time and space complexity on dfs

--- python/depth_first_search.py
+++ python/depth_first_search.py
@@ -7,6 +7,9 @@ class Node:
 		self.children.append(Node(name))
 		return self
 
+
+	# O(v + e) where v is every vertex in the tree, which we have to traverse
+	# and e is each edge, which is an element in the node's children array
 	def depthFirstSearch(self, array):
 		if self is None:
 			return []
