b5287c5e3fdf4a52e84fea88747954442b45398c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-12 11:55:09 -0500

    Finish product sum

--- python/product_sum.py
+++ python/product_sum.py
@@ -1,8 +1,8 @@
-def productSum(array, depth = 1):
+def productSum(array, multiplier = 1):
 	sum = 0
 	for el in array:
-		if type(el) is List:
-			sum += productSum(el, depth + 1):
+		if type(el) is list:
+			sum += productSum(el, multiplier + 1)
 		else:
 			sum += el
-	return sum
+	return sum * multiplier
