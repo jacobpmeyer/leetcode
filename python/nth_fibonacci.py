6dd54561c6adcea21fe469e060e94e4f6901f772
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-12 11:38:31 -0500

    Add O(1) space solution to nth fib

--- python/nth_fibonacci.py
+++ python/nth_fibonacci.py
@@ -8,3 +8,13 @@ def getNthFib(n, seen = {1: 0, 2: 1}):
 		seen[n] = getNthFib(n - 1, seen) + getNthFib(n - 2, seen)
 		return seen[n]
 	
+# O(n) Time | O(1) Space
+def getNthFib(n):
+	lastTwo = [0, 1]
+	counter = 3
+	while counter <= n:
+		newLast = lastTwo[0] + lastTwo[1]
+		lastTwo[0] = lastTwo[1]
+		lastTwo[1] = newLast
+		counter += 1
+	return lastTwo[1] if n > 1 else lastTwo[0] 
