038d7f0ea7d33e5412bdda304518e7cafe624d3c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-13 17:58:36 -0500

    Finish move element to the end

--- python/move_element_to_end.py
+++ python/move_element_to_end.py
@@ -1,3 +1,15 @@
 # O(n) Time | O(1) Space
 def moveElementToEnd(array, toMove):
-	
+	end = len(array) - 1
+	i = 0
+	while i < end:
+		if array[end] == toMove:
+			end -= 1
+			continue
+		if array[i] == toMove:
+			swap(i, end, array)
+		i += 1
+	return array
+
+def swap(i, j, arr):
+	arr[i], arr[j] = arr[j], arr[i]
