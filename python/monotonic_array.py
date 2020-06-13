130eaafe7dddeb04566baf14feda3b4a87d33c01
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-13 18:11:34 -0500

    Finish monotonic array

--- python/monotonic_array.py
+++ python/monotonic_array.py
@@ -8,4 +8,14 @@
 # O(1) Space as we are not creating any new variables that depend on the input
 
 def isMonotonic(array):
-	
+	increasing = False
+	decreasing = False
+	for i in range(1, len(array)):
+		if array[i] > array[i - 1]:
+			increasing = True
+		elif array[i] < array[i - 1]:
+			decreasing = True
+	if increasing and decreasing:
+		return False
+	else:
+		return True
