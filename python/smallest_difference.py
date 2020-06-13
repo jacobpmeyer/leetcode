05877825cd2adc85054678e693a6aaba88ab0fa9
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-13 17:31:02 -0500

    Finish smallest difference

--- python/smallest_difference.py
+++ python/smallest_difference.py
@@ -1,4 +1,25 @@
 # O(nlogn + mlogm) Time | O(1) Space
 
 def smallestDifference(arrayOne, arrayTwo):
-  
+	arrayOne.sort()
+	arrayTwo.sort()
+	i = 0
+	j = 0
+	current = float("inf")
+	smallest = float("inf")
+	difArr = []
+	while i < len(arrayOne) and j < len(arrayTwo):
+		numOne = arrayOne[i]
+		numTwo = arrayTwo[j]
+		if numOne < numTwo:
+			current = numTwo - numOne
+			i += 1
+		elif numOne > numTwo:
+			current = numOne - numTwo
+			j += 1
+		else:
+			return [numOne, numTwo]
+		if current < smallest:
+			smallest = current
+			difArr = [numOne, numTwo]
+	return difArr
