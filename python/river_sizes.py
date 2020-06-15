b091feb8395a8893cd78aa5108f9817583d5a448
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-15 11:16:38 -0500

    Finish river sizes

--- python/river_sizes.py
+++ python/river_sizes.py
@@ -1,10 +1,10 @@
 
 
 def riverSizes(matrix):
-	visited = [[False for x in range(len(matrix[0]))] for y in range(len(matrix))]
-    sizes = []
+	visited = [[False for el in row] for row in matrix]
+	sizes = []
 	for r in range(len(matrix)):
-		for c in range(len(matrix[r])):
+		for c in range(len(matrix[0])):
 			if visited[r][c]:
 				continue
 			explore(r, c, visited, sizes, matrix)
@@ -14,10 +14,27 @@ def explore(r, c, visited, sizes, matrix):
 	riverSize = 0
 	nodesToExplore = [[r, c]]
 	while len(nodesToExplore):
-		# check to see if this works by calling .pop(0)
 		r, c = nodesToExplore.pop()
 		if visited[r][c]:
 			continue
+		visited[r][c] = True
 		if matrix[r][c] == 0:
 			continue
-		unexploredNodes = 
+		riverSize += 1
+		unexploredNodes = getUnexploredNodes(r, c, matrix, visited)
+		for node in unexploredNodes:
+			nodesToExplore.append(node)
+	if riverSize > 0:
+		sizes.append(riverSize)
+
+def getUnexploredNodes(r, c, matrix, visited):
+	unexploredNodes = []
+	if r > 0 and not visited[r - 1][c]:
+		unexploredNodes.append([r - 1, c])
+	if r < len(matrix) - 1 and not visited[r + 1][c]:
+		unexploredNodes.append([r + 1, c])
+	if c > 0 and not visited[r][c - 1]:
+		unexploredNodes.append([r, c - 1])
+	if c < len(matrix[0]) - 1 and not visited[r][c + 1]:
+		unexploredNodes.append([r, c + 1])
+	return unexploredNodes
