3f4cc741f211f1ebc12d0f8278687c424607ab48
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-17 16:45:26 -0500

    Finish kth node

--- python/remove_kth_node_from_the_end.py
+++ python/remove_kth_node_from_the_end.py
@@ -1,3 +1,4 @@
+# This is an input class. Do not edit.
 class LinkedList:
 	def __init__(self, value):
 		self.value = value
@@ -5,4 +6,18 @@ class LinkedList:
 
 
 def removeKthNodeFromEnd(head, k):
-  pass
+	counter = 0
+	first = head
+	second = head
+	while counter < k:
+		counter += 1
+		first = first.next
+	if first is None:
+		head.value = head.next.value
+		head.next = head.next.next
+		return
+	while first.next is not None:
+		first = first.next
+		second = second.next
+	second.next = second.next.next
+	return head
