24695bba2c80b39d0957d7901b28e4f6a58d7549
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-19 16:41:15 -0500

    Add simple solution to best time to buy and sell

--- python/best_time_to_buy_and_sell_stock.py
+++ python/best_time_to_buy_and_sell_stock.py
@@ -27,3 +27,11 @@ class Solution:
 			total += peak - valley
 		# After a single pass through, we return the total
 		return total
+
+	# Same time and space complexity as above, but simpler (kinda)
+	def maxProfit(self, prices: List[int]) -> int:
+		profit = 0
+		for i in range(1, len(prices)):
+			if prices[i] > prices[i - 1]:
+				profit += prices[i] - prices[i - 1]		
+		return profit
