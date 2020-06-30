72c6439a1dcea185f55b4a413615dbd2ad3a4619
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-30 18:39:40 -0500

    Product of array except self O(n) space solution

---
+++ python/dynamic_programming/product_of_array_except_self.py
@@ -0,0 +1,31 @@
+
+# Given an array nums of n integers where n > 1,  return an array output such that 
+# output[i] is equal to the product of all the elements of nums except nums[i].
+
+# Example:
+
+# Input:  [1,2,3,4]
+# Output: [24,12,8,6]
+# Constraint: It's guaranteed that the product of the elements of any prefix or 
+# suffix of the array (including the whole array) fits in a 32 bit integer.
+
+# Note: Please solve it without division and in O(n).
+
+# Follow up:
+# Could you solve it with constant space complexity? (The output array does not 
+# count as extra space for the purpose of space complexity analysis.)
+
+# O(n) Time | O(n) Space where n is the length of the list
+class Solution:
+  def productExceptSelf(self, nums: List[int]) -> List[int]:
+    length = len(nums)
+    left, right, output = [0] * length, [0] * length, [0] * length
+    left[0] = 1
+    right[-1] = 1
+    for i in range(1, length):
+      left[i] = left[i - 1] * nums[i - 1]
+    for i in reversed(range(length - 1)):
+      right[i] = right[i + 1] * nums[i + 1]
+    for i in range(length):
+      output[i] = left[i] * right[i]
+    return output
