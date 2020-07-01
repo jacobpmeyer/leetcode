daffc2c5a22291189f9ce709e46b2bbb9e990eec
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-01 16:06:11 -0500

    Finish min heap construction

--- python/min_heap_contruction.py
+++ python/min_heap_contruction.py
@@ -4,25 +4,45 @@ class MinHeap:
         self.heap = self.buildHeap(array)
 
     def buildHeap(self, array):
-        # Write your code here.
-        pass
+        firstParentIdx = (len(array) - 2) // 2
+		for i in reversed(range(firstParentIdx + 1)):
+			self.siftDown(i, len(array) - 1, array)
+		return array
 
-    def siftDown(self):
-        # Write your code here.
-        pass
+    def siftDown(self, currentIdx, endIdx, heap):
+        childOneIdx = (currentIdx * 2) + 1
+		while childOneIdx <= endIdx:
+			childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else - 1
+			if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+				idxToSwap = childTwoIdx
+			else: 
+				idxToSwap = childOneIdx
+			if heap[idxToSwap] < heap[currentIdx]:
+				self.swap(idxToSwap, currentIdx, heap)
+				currentIdx = idxToSwap
+				childOneIdx = currentIdx * 2 + 1
+			else:
+				return
 
-    def siftUp(self):
-        # Write your code here.
-        pass
+    def siftUp(self, currentIdx, heap):
+        parentIdx = (currentIdx - 1) // 2
+		while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+			self.swap(currentIdx, parentIdx, heap)
+			currentIdx = parentIdx
+			parentIdx = (currentIdx - 1) // 2
 
     def peek(self):
-        # Write your code here.
-        pass
+        return self.heap[0]
 
     def remove(self):
-        # Write your code here.
-        pass
+        self.swap(0, len(self.heap) - 1, self.heap)
+		valueToRemove = self.heap.pop()
+		self.siftDown(0, len(self.heap) - 1, self.heap)
+		return valueToRemove
 
     def insert(self, value):
-        # Write your code here.
-        pass
+		self.heap.append(value)
+		self.siftUp(len(self.heap) - 1, self.heap)
+
+	def swap(self, i, j, heap):
+		heap[i], heap[j] = heap[j], heap[i]
