94864de07cd072cde74c1709cb299046be99324b
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-30 19:10:18 -0500

    Product of array except self O(1) space solution

--- python/dynamic_programming/product_of_array_except_self.py
+++ python/dynamic_programming/product_of_array_except_self.py
@@ -29,3 +29,17 @@ class Solution:
     for i in range(length):
       output[i] = left[i] * right[i]
     return output
+
+
+class Solution:
+  def productExceptSelf(self, nums: List[int]) -> List[int]:
+    length = len(nums)
+    answer = [0]*length
+    answer[0] = 1
+    for i in range(1, length):
+      answer[i] = nums[i - 1] * answer[i - 1]
+    right = 1
+    for i in reversed(range(length)):
+      answer[i] = answer[i] * right
+      right *= nums[i]
+    return answer
