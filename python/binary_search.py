8c57ea232ff6d40ec65b6df01e324db46970eb7d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-02 17:16:53 -0500

    Finish bsearch iterative

---
+++ python/binary_search.py
@@ -0,0 +1,12 @@
+def binarySearch(array, target):
+	left = 0
+	right = len(array) - 1
+	while left <= right:
+		middle = (left + right) // 2
+		if array[middle] == target:
+			return middle
+		elif array[middle] > target:
+			right = middle - 1
+		else:
+			left = middle + 1
+	return - 1
