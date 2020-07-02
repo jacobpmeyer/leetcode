792e4029b5c7cca96773b764cc4a4ff3ad05a664
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-02 13:22:17 -0500

    Finish linked list construction

--- python/linked_list_construction.py
+++ python/linked_list_construction.py
@@ -13,33 +13,86 @@ class DoublyLinkedList:
         self.tail = None
 
     def setHead(self, node):
-        # Write your code here.
-        pass
+        if self.head is None:
+			self.head = node
+		else:
+			self.head.prev = node
+			node.next = self.head
+			self.head = node
 
     def setTail(self, node):
-        # Write your code here.
-        pass
+        if self.tail is None:
+			self.tail = node
+			self.tail.prev = self.head
+			if self.head is not None:
+				self.head.next = node
+		else:
+			self.tail.next = node
+			node.prev = self.tail
+			self.tail = node
 
     def insertBefore(self, node, nodeToInsert):
-        # Write your code here.
-        pass
+        if node == self.head:
+			self.setHead(nodeToInsert)
+		elif node == self.tail:
+			self.setTail(nodeToInsert)
+		else:
+			node.prev.next = nodeToInsert
+			nodeToInsert.next = node
+			nodeToInsert.prev = node.prev
+			node.prev = nodeToInsert
 
     def insertAfter(self, node, nodeToInsert):
-        # Write your code here.
-        pass
+        if node == self.head:
+			self.setHead(nodeToInsert)
+		elif node == self.tail:
+			self.setTail(nodeToInsert)
+		else:
+			node.next.prev = nodeToInsert
+			nodeToInsert.next = node.next
+			nodeToInsert.prev = node
+			node.next = nodeToInsert
 
     def insertAtPosition(self, position, nodeToInsert):
-        # Write your code here.
-        pass
+        currentPos = 1
+		currentNode = self.head
+		while currentPos < position and currentNode is not None:
+			currentNode = currentNode.next
+		nodeToInsert.prev = currentNode.prev
+		nodeToInsert.next = currentNode
+		if currentNode.prev:
+			currentNode.prev.next = nodeToInsert
+			currentNode.prev = nodeToInsert
 
     def removeNodesWithValue(self, value):
-        # Write your code here.
-        pass
+        currentNode = self.head
+		removedNodes = []
+		while currentNode is not None:
+			if value == currentNode.value:
+				if currentNode.prev:
+					currentNode.prev.next = currentNode.next
+				if currentNode.next:
+					currentNode.next.prev = currentNode.prev
+				removedNodes.append(currentNode)
+			currentNode = currentNode.next
+
 
     def remove(self, node):
-        # Write your code here.
-        pass
+        currentNode = self.head
+		while currentNode is not None:
+			if node == currentNode:
+				if node.prev:
+					node.prev.next = node.next
+				if node.next:
+					node.next.prev = node.prev
+				return node
+			else:
+				currentNode = currentNode.next
 
     def containsNodeWithValue(self, value):
-        # Write your code here.
-        pass
+        currentNode = self.head
+		while currentNode is not None:
+			if currentNode.value == value:
+				return true
+			else:
+				currentNode = currentNode.next
