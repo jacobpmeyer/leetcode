383ef561f38f9972fd9a9e4bcd57dba7bb49cc9d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-03 18:14:46 -0500

    Finish balanced brackets

--- python/balanced_buckets.py
+++ python/balanced_buckets.py
@@ -1,3 +1,6 @@
+
+# O(n) Time | O(n) Space where n is the length of the input string
+# My solution
 def balancedBrackets(string):
 	lastSeen = []
 	for char in string:
@@ -19,3 +22,21 @@ def balancedBrackets(string):
 		elif char == "(" or char == "[" or char == "{":
 			lastSeen.append(char)
 	return len(lastSeen) == 0
+
+# ae solution
+def balancedBrackets(string):
+	openingBrackets = "([{"
+	closingBrackets = "})]"
+	matchingBrackets = { "}": "{", ")": "(", "]": "[" }
+	stack = []
+	for char in string:
+		if char in openingBrackets:
+			stack.append(char)
+		elif char in closingBrackets:
+			if len(stack) == 0:
+				return False
+			if stack[-1] == matchingBrackets[char]:
+				stack.pop()
+			else:
+				return False
+	return len(stack) == 0
