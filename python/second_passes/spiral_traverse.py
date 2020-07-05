319ff205c1255c823b150a9864cafb1e90e6f107
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-05 17:43:44 -0500

    Finish spiral traverse again

--- python/second_passes/spiral_traverse.py
+++ python/second_passes/spiral_traverse.py
@@ -1,29 +1,22 @@
 def spiralTraverse(array):
-    output = [array[0]]
-    size = len(array) * len(array[0])
-    row = 0
-    col = 1
-    currentLen = len(array)
-    currentMin = 0
-    while len(output) < size:
-        while col < currentLen:
-            output.append(array[row][col])
-            col += 1
-        col -= 1
-        while row < currentLen:
-            output.append(array[row][col])
-            row += 1
-        row -= 1
-        col -= 1
-        while col >= currentMin:
-            output.append(array[row][col])
-            col -= 1
-        col += 1
-        while row >= currentMin:
-            output.append(array[row][col])
-            row -= 1
-        currentMin += 1
-        currentLen -= 1
-        row = currentMin
-        col = currentMin
+    output = []
+    startRow, endRow = 0, len(array) - 1
+    startCol, endCol = 0, len(array[0]) - 1
+    while startRow <= endRow and startCol <= endCol:
+        for col in range(startCol, endCol + 1):
+            output.append(array[startRow][col])
+        for row in range(startRow + 1, endRow + 1):
+            output.append(array[row][endCol])
+        for col in reversed(range(startCol, endCol)):
+            if startRow == endRow:
+                break
+            output.append(array[endRow][col])
+        for row in reversed(range(startRow + 1, endRow)):
+            if startCol == endCol:
+                break
+            output.append(array[row][startCol])
+        startRow += 1
+        startCol += 1
+        endRow -= 1
+        endCol -= 1
     return output
