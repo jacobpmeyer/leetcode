aef032e98e6ff22afff6ee1f0510784ff16531ec
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-06 10:53:31 -0500

    Finish number of ways to make change

--- python/second_passes/number_of_ways_to_make_change.py
+++ python/second_passes/number_of_ways_to_make_change.py
@@ -1,5 +1,11 @@
 def numberOfWaysToMakeChange(n, denoms):
-
+    ways = [0 for x in range(n + 1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+    return ways[-1] 
 
 test = {"denoms": [2, 4], "n": 7}
 print(numberOfWaysToMakeChange(test["n"], test["denoms"]))
