262381deb40c4809bc8523abd1a4604a0f7a2d82
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-06 12:21:44 -0500

    Finish kadanes

--- python/second_passes/kadanes.py
+++ python/second_passes/kadanes.py
@@ -1,2 +1,8 @@
 def kadanesAlgorithm(array):
-    
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(maxEndingHere + num, num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
