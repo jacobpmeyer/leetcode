5bcf8eebe29007586c807d15acd4bcfbb41c9175
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-07 16:35:49 -0500

    Start rotate array

--- python/rotate_array.py
+++ python/rotate_array.py
@@ -1,11 +1,6 @@
-
-# O(n) Time where n is the length of the array.
-# O(1) Space
-# Not optimal
 class Solution:
-	def rotate(self, nums: List[int], k: int) -> None:
-		pivot = -(k % len(nums))
-        nums[:] = nums[pivot:] + nums[:pivot]
-		"""
-		Do not return anything, modify nums in-place instead.
-		"""
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
