319041d79d9c3e7e511c21dfce426c03a990880f
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-08 11:27:28 -0500

    Finish contains duplicates

--- python/contains_duplicate.py
+++ python/contains_duplicate.py
@@ -1,3 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
+        numsDict = {}
+        for num in nums:
+            if num in numsDict:
+                return True
+            else:
+                numsDict[num] = True
+        return False
