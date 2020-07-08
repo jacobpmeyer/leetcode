572396c3243ec7fe9d7dfdaeedc5b6154eff1863
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-08 14:24:35 -0500

    Finish intersection of two arrays

--- python/intersection_of_two_arrays.py
+++ python/intersection_of_two_arrays.py
@@ -2,10 +2,15 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         shortArr = nums1 if len(nums2) > len(nums1) else nums2
         longArr = nums2 if len(nums2) > len(nums1) else nums1
-        shortDict = {}
-        for i, num in enumerate(shortArr):
-            if num in shortDict:
-                shortDict[num].append(i)
+        longDict = {}
+        intersections = []
+        for i, num in enumerate(longArr):
+            if num in longDict:
+                longDict[num] += 1
             else:
-                shortDict[num] = [i]
-        
+                longDict[num] = 1
+        for num in shortArr:
+            if num in longDict and longDict[num] > 0:
+                intersections.append(num)
+                longDict[num] -= 1
+        return intersections
