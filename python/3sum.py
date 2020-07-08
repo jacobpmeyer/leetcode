373975faaebd4be36dbb5b4413c4e15ebc4369b6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-08 10:56:57 -0500

    Finish 3 sum

--- python/3sum.py
+++ python/3sum.py
@@ -1,3 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        sumsList = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                potentialMatch = [nums[i], nums[left], nums[right]]
+                potentialSum = nums[i] + nums[left] + nums[right]
+                if potentialSum < 0:
+                    left += 1
+                elif potentialSum > 0:
+                    right -= 1
+                else:
+                    if not len(sumsList) or sumsList[-1] != potentialMatch:
+                        sumsList.append(potentialMatch)
+                    left += 1
+                    right -= 1
+        return sumsList
