8c4b9ae73539a012a26612003842037cd2bb7f9b
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-08 19:05:36 -0500

    Finish move zeroes

--- python/move_zeroes.py
+++ python/move_zeroes.py
@@ -3,4 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        i = 0
+        j = 1
+        while i < len(nums) and j < len(nums):
+            if nums[i] == 0:
+                while nums[j] == 0 or i == j:
+                    j += 1
+                    if j >= len(nums):
+                        return nums
+                nums[i], nums[j] = nums[j], nums[i]
+            i += 1
