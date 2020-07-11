031194406837035f54272cf773c69f9b195a2d06
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-11 14:02:41 -0500

    Finish valid sudoku

--- python/powerset.py
+++ python/powerset.py
@@ -1,17 +1,17 @@
 
 # Recursive approach
 # O(n*n^2) Time | O(n*n^2) Space
-def powerset(array, idx = None):
-	if idx is None:
-		idx = len(array) - 1
-	if idx < 0:
-		return [[]]
-	ele = array[idx]
-	subsets = powerset(array, idx - 1)
-	for i in range(len(subsets)):
-		currentSubset = subsets[i]
-		subsets.append(currentSubset + [ele])
-	return subsets
+# def powerset(array, idx = None):
+# 	if idx is None:
+# 		idx = len(array) - 1
+# 	if idx < 0:
+# 		return [[]]
+# 	ele = array[idx]
+# 	subsets = powerset(array, idx - 1)
+# 	for i in range(len(subsets)):
+# 		currentSubset = subsets[i]
+# 		subsets.append(currentSubset + [ele])
+# 	return subsets
 
 # Iterative approach
 # O(n*n^2) Time | O(n*n^2) Space
@@ -22,3 +22,14 @@ def powerset(array):
 			currentSubset = subsets[i]
 			subsets.append(currentSubset + [ele])
 	return subsets
+
+
+
+array = [1, 2, 3]
+# => [1, 2, 3] => el = 3
+# => [1, 2] => el = 2
+# => [1] => el = 1
+# => [] => return up []
+# => subsets = [] => currentSub = subsets[0] => subsets.append(current + [el])
+# => subsets = [[],[1]] el = 2 => subsets.append([] + [2] ) 
+print(powerset(array))
