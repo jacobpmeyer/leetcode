a8fed4fe8e12f8fe0598cfd9e12055b5e158de21
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-12 17:26:44 -0500

    Finish first unique character in string

--- python/first_unique_character_in_string.py
+++ python/first_unique_character_in_string.py
@@ -1,2 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        chars = {}
+        for i, char in enumerate(s):
+            chars[char] = chars.get(char, 0) + 1
+        for i, char in enumerate(s):
+            if chars[char] == 1:
+                return i
+        return -1
