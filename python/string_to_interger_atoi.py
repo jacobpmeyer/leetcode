e1cc2ff6d131d3487a3727dc035fde523fc3d1db
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-12 20:44:56 -0500

    Finish string to integer atoi

--- python/string_to_interger_atoi.py
+++ python/string_to_interger_atoi.py
@@ -1,26 +1,31 @@
 class Solution:
     def myAtoi(self, string: str) -> int:
-        maxPos = 2**31
+        maxPos = 2**31 - 1
         minPos = -2**31
         nums = "0123456789"
-        # left = 0
-        # while left < len(string) and string[left] not in nums:
-        #     left += 1
-        # if left > 0:
-        #     if string[left - 1] not in nums and string[left - 1] != "-":
-        #         return 0
-        # right = left
-        # while right < len(string) and string[right] in nums:
-        #     right += 1
-        string.strip()
-        # strInt = int(string[left:right])
-        strInt = int(string)
+        left = 0
+        plusMinus = ("-", "+", " ")
+        if string == "":
+            return 0
+        string = string.strip()
+        if len(string) == 2 and string[0] not in nums and string[0] not in plusMinus:
+            return 0
+        while left < len(string) and string[left] not in nums:
+            if left > 0 and string[left - 1] not in nums:
+                return 0
+            left += 1
+        if left > 0 and string[left - 1] not in nums and string[left - 1] not in plusMinus:
+            return 0
+        right = left
+        while right < len(string) and string[right] in nums:
+            right += 1
+        if string[left:right] == "":
+            return 0
+        strInt = int(string[left:right])
+        if left > 0 and string[left - 1] == "-":
+            strInt = -strInt
+        if strInt > maxPos:
+            return maxPos
+        if strInt < minPos:
+            return minPos
         return strInt
-        # if string[left - 1] == "-":
-        
-        #     strInt = -strInt
-        # if strInt > maxPos:
-        #     return maxPos
-        # if strInt < minPos:
-        #     return minPos
-        # return strInt
