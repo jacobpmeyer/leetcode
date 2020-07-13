d1826da52bbc8e844dee6704663accee82bde4e6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-13 12:37:48 -0500

    Finish same tree

--- python/same_tree.py
+++ python/same_tree.py
@@ -6,3 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if q is None and p is None:
+            return True
+        if (p is None and q is not None) or (q is None and p is not None):
+            return False
+        if q.val!= p.val:
+            return False
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return left and right
