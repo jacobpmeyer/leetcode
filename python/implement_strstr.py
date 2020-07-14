84164f454bc2366fae886177b87547a0103097da
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 15:21:59 -0500

    Finish implement strstr

--- python/implement_strstr.py
+++ python/implement_strstr.py
@@ -1,2 +1,21 @@
+# h is the length of the haystack, n is the length of the needle
+# O((h - n)n) Time | O(1) Space
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        n, h = len(needle), len(haystack)
+        if n == 0:
+            return 0
+        ph = 0
+        while ph < h - n + 1:
+            while ph < h - n + 1 and haystack[ph] != needle[0]:
+                ph += 1
+            pn = 0
+            currLen = 0
+            while ph < h and pn < n and haystack[ph] == needle[pn]:
+                pn += 1
+                currLen += 1
+                ph += 1
+            if currLen == n:
+                return ph - currLen
+            ph = ph - currLen + 1
+        return -1
