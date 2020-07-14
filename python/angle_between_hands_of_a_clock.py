072a835c1a477b5cff2b6af03497db91fe061792
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 11:38:55 -0500

    Finish angle between hands of a clock

--- python/angle_between_hands_of_a_clock.py
+++ python/angle_between_hands_of_a_clock.py
@@ -1,2 +1,12 @@
 class Solution:
     def angleClock(self, hour: int, minutes: int) -> float:
+        if hour == 12:
+            hour = 0
+        newHour = hour * 30
+        newMinutes = minutes * 6
+        hourCalc = newHour + (minutes / 2)
+        res = abs(hourCalc - newMinutes)
+        if res > 180:
+            dif = res - 180
+            res = res - (dif * 2)
+        return res
