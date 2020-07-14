2d03dbfdb47679f4ba486d3056bc55162a4c66ba
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 11:57:46 -0500

    Finish valid anagram

--- python/valid_anagram.py
+++ python/valid_anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+        first = {}
+        second = {}
+        for char in s:
+            first[char] = first.get(char, 0) + 1
+        for char in t:
+            second[char] = second.get(char, 0) + 1
+        return first == second
