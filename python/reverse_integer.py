d7373315d7dba3262805fb59265470a60b8ce9ae
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 11:49:43 -0500

    Finish reverse integer

--- python/reverse_integer.py
+++ python/reverse_integer.py
@@ -1,2 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        positive = x >= 0
+        answer = 0
+        if not positive:
+            x += -(x) * 2
+        while x > 9:
+            answer = (10 * answer) + (x % 10)
+            x = x // 10
+        answer = (10 * answer) + (x % 10)
+        if not positive:
+            answer = -(answer)
+        if answer > 2**31 - 1 or answer < -2**31:
+            return 0
+        else:
+            return answer
