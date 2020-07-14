08189a9a7c0947ce255fd12ea196d30dfbd8d9f2
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 14:07:34 -0500

    Finish valid palindrome

--- python/valid_palindrome.py
+++ python/valid_palindrome.py
@@ -1,4 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         alpha = "abcdefghijklmnopqrstuvwxyz"
-        
+        l = []
+        for char in s:
+            if char.lower() not in alpha:
+                continue
+            l.append(char)
+        print(l)
+        print(l[::-1])
+        return l == l[::-1]
+
+s = Solution()
+print(s.isPalindrome("poo,54::::     p"))
+print(s.isPalindrome("racecar"))
