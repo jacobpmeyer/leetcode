40ce9762d0093a9fbd211bf21f6820dd08ffbf06
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-14 19:23:01 -0500

    Work on implement strstr

--- python/implement_strstr.py
+++ python/implement_strstr.py
@@ -19,3 +19,49 @@ class Solution:
                 return ph - currLen
             ph = ph - currLen + 1
         return -1
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        L, n = len(needle), len(haystack)
+        if L > n:
+            return -1
+        
+        # base value for the rolling hash function
+        a = 26
+        # modulus value for the rolling hash function to avoid overflow
+        modulus = 2**31
+        
+        # lambda-function to convert character to integer
+        h_to_int = lambda i : ord(haystack[i]) - ord('a')
+        needle_to_int = lambda i : ord(needle[i]) - ord('a')
+        
+        # compute the hash of strings haystack[:L], needle[:L]
+        h = ref_h = 0
+        for i in range(L):
+            h = (h * a + h_to_int(i)) % modulus
+            ref_h = (ref_h * a + needle_to_int(i)) % modulus
+            print(h)
+            print(ref_h)
+        if h == ref_h:
+            return 0
+        print("==========")
+        # const value to be used often : a**L % modulus
+        aL = pow(a, L, modulus) 
+        for start in range(1, n - L + 1):
+            # compute rolling hash in O(1) time
+            h = (h * a - h_to_int(start - 1) * aL + h_to_int(start + L - 1)) % modulus
+            print(h)
+            if h == ref_h:
+                return start
+
+        return -1
+
+needle = "cat"
+haystack = "tacocat"
+s = Solution()
+print(s.strStr(haystack, needle))
+
+
+12846 * 26 - 19 * pow(26, 3, 2**31) + 14 % 2**31
+
+(12846 * 26) - 19 * pow(26, 3, 2**31) + 14 % 2**31
