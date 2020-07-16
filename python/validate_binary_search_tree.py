833496ebef82951c11fe889fd8190bcbcbd34d65
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-16 18:16:56 -0500

    Finish validate bst

--- python/validate_binary_search_tree.py
+++ python/validate_binary_search_tree.py
@@ -6,3 +6,17 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        left = self.bstHelper(root.left, float("-inf"), root.val)
+        right = self.bstHelper(root.right, root.val, float("inf"))
+        return left and right
+
+    def bstHelper(self, node: TreeNode, minVal: int, maxVal: int) -> bool:
+        if node is None:
+            return True
+        if node.val <= minVal or node.val >= maxVal:
+            return False
+        left = self.bstHelper(node.left, minVal, node.val)
+        right = self.bstHelper(node.right, node.val, maxVal)
+        return left and right
