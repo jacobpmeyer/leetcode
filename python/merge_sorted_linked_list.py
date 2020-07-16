bc55332af4a49fb0fa857284ac35e3f1f83c6c53
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-16 16:40:40 -0500

    Finish merge sorted linked list

--- python/merge_sorted_linked_list.py
+++ python/merge_sorted_linked_list.py
@@ -5,3 +5,15 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        preHead = ListNode()
+        prev = preHead
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l2 is None else l2
+        return preHead.next
