caeb7005035400017af3e88f6578b651d10d7c94
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-16 14:10:18 -0500

    Finish reverse linked list

--- python/reverse_linked_list.py
+++ python/reverse_linked_list.py
@@ -5,4 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        
+        node = head
+        prev = None
+        while node is not None:
+            tempNext = node.next
+            node.next = prev
+            prev = node
+            node = tempNext
+        return prev
