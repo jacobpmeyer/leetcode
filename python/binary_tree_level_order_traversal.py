9ff37ccc77d7c3744a97e687ca17e75ec730762f
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-16 19:42:03 -0500

    Finish binary tree level order traversal

--- python/binary_tree_level_order_traversal.py
+++ python/binary_tree_level_order_traversal.py
@@ -6,3 +6,14 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        return self.levelHelper(root, [], 0)
+
+    def levelHelper(self, node, arr, lvl):
+        if node is None:
+            return arr
+        while len(arr) < lvl + 1:
+            arr.append([])
+        arr[lvl].append(node.val)
+        self.levelHelper(node.left, arr, lvl + 1)
+        self.levelHelper(node.right, arr, lvl + 1)
+        return arr
