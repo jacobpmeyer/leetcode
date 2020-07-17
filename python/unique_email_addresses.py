c475fb0322766a6fc29aeccf2dcf82aa2535ebc3
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-17 13:17:14 -0500

    Finish unique email addresses

--- python/unique_email_addresses.py
+++ python/unique_email_addresses.py
@@ -1,3 +1,13 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        
+        d = {}
+        for email in emails:
+            parts = email.split("@")
+            newStr = parts[0].replace(".", "").split("+")[0]
+            if parts[1] not in d:
+                d[parts[1]] = set()
+            d[parts[1]].add(newStr)
+        count = 0
+        for i in d:
+            count += len(d[i])
+        return count
