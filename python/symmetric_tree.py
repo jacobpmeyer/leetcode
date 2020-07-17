90e9f22581d158e35268b591df7dbd1f291f670c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-16 19:26:36 -0500

    Finish symmetric tree

--- python/symmetric_tree.py
+++ python/symmetric_tree.py
@@ -4,7 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        if root is None:
+        return self.isMirror(root, root)
+    
+    def isMirror(self, node1, node2):
+        if node1 is None and node2 is None:
             return True
+        if node1 is None or node2 is None:
+            return False
+        one = self.isMirror(node1.right, node2.left)
+        two = self.isMirror(node2.right, node1.left)
+        return (node1.val == node2.val) and one and two
