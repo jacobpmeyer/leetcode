1ba0ff63f93551496b76fbb4c44c332bb49ec7a4
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-17 18:16:48 -0500

    Finish first bad version

--- python/first_bad_version.py
+++ python/first_bad_version.py
@@ -9,3 +9,17 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        def helper(min, max, vers):
+            if min > max:
+                return min
+            mid = (min + max) // 2
+            vers[n] = isBadVersion(mid)
+            if vers[n] and (n - 1) in vers and not vers[n - 1]:
+                return mid
+            elif vers[n]:
+                return helper(min, mid - 1, vers)
+            else:
+                return helper(mid + 1, max, vers)
+            
+        vers = {}
+        return helper(0, n, vers)
