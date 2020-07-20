0f95274a7cfdf2aa956ecb9b02b32b8e2a0fdced
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-20 14:17:23 -0500

    Finish climbing stairs

--- python/climbing_stairs.py
+++ python/climbing_stairs.py
@@ -1,3 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        def stairHelper(num, mem):
+            if num <= 2:
+                return num
+            if num in mem:
+                return mem[num]
+            mem[num] = stairHelper(num - 1, mem) + stairHelper(num - 2, mem)
+            return mem[num]
+        mem = {}
+        return stairHelper(n, mem)
