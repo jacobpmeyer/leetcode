f3066513c7e4bbe5396b2839edf7dce679f0bc0d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-20 14:35:19 -0500

    Finish best time to buy and sell stocks

--- python/best_time_to_buy_and_sell_stock.py
+++ python/best_time_to_buy_and_sell_stock.py
@@ -1,3 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        maxProf = 0
+        minProf = float("inf")
+        for i in range(len(prices)):
+            if prices[i] < minProf:
+                minProf = prices[i]
+            elif prices[i] - minProf > maxProf:
+                maxProf = prices[i] - minProf
+        return maxProf
