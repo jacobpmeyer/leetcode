359d10ffed4ada106b83716845d4eef59712dc29
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-20 15:01:33 -0500

    Finish house robber

--- python/house_robber.py
+++ python/house_robber.py
@@ -1,3 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        if not len(nums):
+            return 0
+        elif len(nums) < 2:
+            return nums[0]
+        arr = [0 for num in nums]
+        arr[0] = nums[0]
+        arr[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            arr[i] = max(nums[i] + arr[i - 2], arr[i - 1])
+        return arr[-1]
