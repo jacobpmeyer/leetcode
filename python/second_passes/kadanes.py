599544473c01a71090cce264c9cf87e3f80eea0a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-21 18:47:39 -0500

    Start validate bst

--- python/second_passes/kadanes.py
+++
@@ -1,8 +0,0 @@
-def kadanesAlgorithm(array):
-    totalMax = array[0]
-    currentMax = array[0]
-    for i in range(1, len(array)):
-        num = array[i]
-        currentMax = max(currentMax + num, num)
-        totalMax = max(currentMax, totalMax)
-    return totalMax
---
+++ python/second_passes/validate_bst.py
@@ -0,0 +1,11 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree):
+    # Write your code here.
+    pass
