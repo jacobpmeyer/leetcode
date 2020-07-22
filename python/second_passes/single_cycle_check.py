7e2b74ab10ce0c2d11e432139d6de6841cd79cd8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-22 14:25:05 -0500

    Finish single cycle check

--- python/second_passes/single_cycle_check.py
+++ python/second_passes/single_cycle_check.py
@@ -1,3 +1,13 @@
 def hasSingleCycle(array):
-    # Write your code here.
-    pass
+    count = 0
+    i = 0
+    while True:
+        i += array[i]
+        count += 1
+        if i < 0 or i >= len(array):
+            i %= len(array)
+        if count >= len(array) or i == 0:
+            if count >= len(array) and i == 0:
+                return True
+            else:
+                return False
