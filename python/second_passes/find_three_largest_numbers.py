e10fb1bb8ac3c53b5b882f8864a6a83403bcf64e
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-22 14:45:47 -0500

    Finish find three largest numbers

--- python/second_passes/find_three_largest_numbers.py
+++ python/second_passes/find_three_largest_numbers.py
@@ -1,3 +1,12 @@
 def findThreeLargestNumbers(array):
-    # Write your code here.
-    pass
+    largest = [float("-inf") for x in range(3)]
+    for num in array:
+        for i in reversed(range(3)):
+            if num > largest[i]:
+                j = 0
+                while j < i:
+                    largest[j] = largest[j + 1]
+                    j += 1
+                largest[i] = num
+                break
+    return largest
