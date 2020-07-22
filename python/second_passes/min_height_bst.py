5a22171a4b9cb12c66a13f863239bd55d06d005e
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-21 19:11:24 -0500

    Finish min height bst

--- python/second_passes/min_height_bst.py
+++ python/second_passes/min_height_bst.py
@@ -1,5 +1,14 @@
 def minHeightBst(array):
-    pass
+    return bstHelper(array, 0, len(array) - 1)
+    
+def bstHelper(array, startIdx, endIdx):
+    if startIdx > endIdx:
+        return
+    mid = (startIdx + endIdx) // 2
+    root = BST(array[mid])
+    root.left = bstHelper(array, startIdx, mid - 1)
+    root.right = bstHelper(array, mid + 1, endIdx)
+    return root
 
 
 class BST:
