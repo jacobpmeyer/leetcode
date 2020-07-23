201051533f3a5a33553ba11325b25544f6be63af
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-23 18:36:50 -0500

    Finish nth fib

--- python/second_passes/nth_fib.py
+++ python/second_passes/nth_fib.py
@@ -1,3 +1,8 @@
-
-
-    
+def getNthFib(n, memo = {}):
+    if n <= 2:
+		return n - 1
+	if n in memo:
+		return memo[n]
+	memo[n - 1] = getNthFib(n - 1, memo)
+	memo[n - 2] = getNthFib(n - 2, memo)
+	return memo[n - 1] + memo[n - 2]
