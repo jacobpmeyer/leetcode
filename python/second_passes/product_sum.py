69f2e13337c54c9a63a68e544ed3cf8d6633f23a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-23 18:43:44 -0500

    Finish product sum

--- python/second_passes/product_sum.py
+++ python/second_passes/product_sum.py
@@ -1,3 +1,12 @@
 # Tip: You can use the type(element) function to check whether an item
 # is a list or an integer.
-def productSum(array):
+def productSum(array, depth = 1):
+    if not len(array):
+        return 0
+    total = 0
+    for el in array:
+        if type(el) is list:
+            total += productSum(el, depth + 1)
+        else:
+            total += el
+    return total * depth
