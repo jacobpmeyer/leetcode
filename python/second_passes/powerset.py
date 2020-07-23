54d70a17a0db95f2c4bc071f70f7f77ef142e4a3
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-23 18:50:31 -0500

    Finish powerset iterative

--- python/second_passes/powerset.py
+++ python/second_passes/powerset.py
@@ -1,3 +1,7 @@
 def powerset(array):
-    # Write your code here.
-    pass
+    sets = [[]]
+    for el in array:
+        for i in range(len(sets)):
+            newSub = sets[i] + [el]
+            sets.append(newSub)
+    return sets
