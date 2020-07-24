d5c95f60d3fc434d07daea87990746a41e14a35e
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-23 19:02:02 -0500

    Finish powerset recursive

--- python/second_passes/powerset.py
+++ python/second_passes/powerset.py
@@ -5,3 +5,14 @@ def powerset(array):
             newSub = sets[i] + [el]
             sets.append(newSub)
     return sets
+
+def powerset(array, idx = None):
+    if idx is None:
+        idx = len(array) - 1
+    if idx < 0:
+        return [[]]
+    subs = powerset(array, idx - 1)
+    for i in range(len(subs)):
+        newSub = subs[i] + [array[idx]]
+        subs.append(newSub)
+    return subs
