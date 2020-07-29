b2309d282f6cc085b51a2e5010e11d1b62d657e6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-28 19:03:08 -0500

    Finish merge sort js

--- javascript/merge_sort.js
+++ javascript/merge_sort.js
@@ -1,6 +1,35 @@
 function mergeSort(array) {
-  copyArray = array.split();
-  return mergeHelper(array, 0, array.length - 1);
+  if (array.length <= 1) return array;
+  const auxArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxArray);
+  return array;
+}
+
+function mergeSortHelper(mainArray, startIdx, endIdx, auxArray) {
+  if (startIdx === endIdx) return;
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  mergeSortHelper(auxArray, startIdx, midIdx, mainArray);
+  mergeSortHelper(auxArray, midIdx + 1, endIdx, mainArray);
+  doMerge(mainArray, startIdx, midIdx, endIdx, auxArray);
+}
+
+function doMerge(mainArray, startIdx, midIdx, endIdx, auxArray) {
+  let k = startIdx;
+  let i = startIdx;
+  let j = midIdx + 1;
+  while (i <= midIdx && j <= endIdx) {
+    if (auxArray[i] <= auxArray[j]) {
+      mainArray[k++] = auxArray[i++];
+    } else {
+      mainArray[k++] = auxArray[j++];
+    }
+  }
+  while (i <= midIdx) {
+    mainArray[k++] = auxArray[i++];
+  }
+  while (j <= endIdx) {
+    mainArray[k++] = auxArray[j++];
+  }
 }
 
 // Do not edit the line below.
