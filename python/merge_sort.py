46d82c8151c4889c224cff67abd7fe81696b6feb
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-28 19:50:38 -0500

    Finish merge sort python

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -1,3 +1,35 @@
 def mergeSort(array):
-    # Write your code here.
-    pass
+    if len(array) <= 1:
+        return array
+    auxArray = array[:]
+    mergeHelper(array, 0, len(array) - 1, auxArray)
+    return array
+
+def mergeHelper(mainArray, startIdx, endIdx, auxArray):
+    if startIdx == endIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    mergeHelper(auxArray, startIdx, midIdx, mainArray)
+    mergeHelper(auxArray, midIdx + 1, endIdx, mainArray)
+    doMerge(mainArray, startIdx, midIdx, endIdx, auxArray)
+
+def doMerge(mainArray, startIdx, midIdx, endIdx, auxArray):
+    k = startIdx
+    i = startIdx
+    j = midIdx + 1
+    while i <= midIdx and j <= endIdx:
+        if auxArray[i] <= auxArray[j]:
+            mainArray[k] = auxArray[i]
+            i += 1
+        else:
+            mainArray[k] = auxArray[j]
+            j += 1
+        k += 1
+    while i <= midIdx:
+        mainArray[k] = auxArray[i]
+        k += 1
+        i += 1
+    while j <= endIdx:
+        mainArray[k] = auxArray[j]
+        k += 1
+        j += 1
