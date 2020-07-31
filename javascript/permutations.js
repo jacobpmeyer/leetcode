97493bf5b0b75f4848f9b7ad24daf25c251a9aca
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-31 10:51:15 -0500

    Finish permutations js

---
+++ javascript/permutations.js
@@ -0,0 +1,26 @@
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(0, array, permutations);
+  return permutations;
+}
+
+function permutationsHelper(i, array, permutations) {
+  if (i == array.length - 1) {
+    permutations.push(array.slice());
+  } else {
+    for (let j = i; j < array.length; j++) {
+      swap(i, j, array);
+      permutationsHelper(i + 1, array, permutations);
+      swap(i, j, array);
+    }
+  }
+}
+
+function swap(i, j, array) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+let array = [1, 2, 3];
+console.log(getPermutations(array));
