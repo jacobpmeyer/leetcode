1149394badda68b6282c4f5cf63f89b0e77cbab6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-07-31 10:38:06 -0500

    Finish permutations python

--- python/second_passes/perms.py
+++ python/second_passes/perms.py
@@ -1,3 +1,31 @@
+# def getPermutations(array):
+#     permutations = []
+#     permutationsHelper(array, [], permutations)
+#     return permutations
+
+# def permutationsHelper(array, currentPermutation, permutations):
+#     if not len(array) and len(currentPermutation):
+#         permutations.append(currentPermutation)
+#     else:
+#         for i in range(len(array)):
+#             newArray = array[:i] + array[i + 1:]
+#             newPermutation = currentPermutation + [array[i]]
+#             permutationsHelper(newArray, newPermutation, permutations)
+
+
 def getPermutations(array):
-    # Write your code here.
-    pass
+    permutations = []
+    permutationsHelper(0, array, permutations)
+    return permutations
+
+def permutationsHelper(i, array, permutations):
+    if i == len(array) - 1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            swap(i, j, array)
+            permutationsHelper(i + 1, array, permutations)
+            swap(i, j, array)
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
