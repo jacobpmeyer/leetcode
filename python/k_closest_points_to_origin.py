1dc7e8df5ae0aa3d18eed38a6ed40ea774158287
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-01 15:49:07 -0500

    k closest using sort

--- python/k_closest_points_to_origin.py
+++ python/k_closest_points_to_origin.py
@@ -1,4 +1,4 @@
-import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        points.sort()
+        points.sort(key = lambda point: point[0]**2 + point[1]**2)
+        return points[:K]
