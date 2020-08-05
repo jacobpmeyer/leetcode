293723c10e4e4b79d0f739446a0b57f5a11b8445
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-05 16:26:05 -0500

    Start remove kth node from end js

---
+++ javascript/remove_kth_node_from_end.js
@@ -0,0 +1,13 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeKthNodeFromEnd(head, k) {}
+
+// Do not edit the line below.
+exports.LinkedList = LinkedList;
+exports.removeKthNodeFromEnd = removeKthNodeFromEnd;
