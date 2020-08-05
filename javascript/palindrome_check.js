a9d272fdd19d218f6af13b98c04fd957966f3ef7
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-05 18:50:38 -0500

    Finish palindrome check js

--- javascript/palindrome_check.js
+++ javascript/palindrome_check.js
@@ -1,6 +1,20 @@
 function isPalindrome(string) {
-  // Write your code here.
+  let l = 0;
+  let r = string.length - 1;
+  while (l < r) {
+    if (string[l] !== string[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
 }
+
+const string = "tacocat";
+const string2 = "hello";
+console.log(isPalindrome(string));
+console.log(isPalindrome(string2));
 
 // Do not edit the line below.
 exports.isPalindrome = isPalindrome;
