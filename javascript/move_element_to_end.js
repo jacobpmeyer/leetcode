59b655cfc0f3a40b8a452fd9747ff0487bd394be
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-05 18:33:38 -0500

    Do move element to end again

--- javascript/move_element_to_end.js
+++ javascript/move_element_to_end.js
@@ -2,21 +2,27 @@ function moveElementToEnd(array, toMove) {
   let l = 0;
   let r = array.length - 1;
   while (l < r) {
-    while (l < r && array[r] === toMove) r--;
-    if (array[l] === toMove) swap(array, l, r);
+    while (l < r && array[r] === toMove) {
+      r--;
+    }
+    if (array[l] === toMove) {
+      swap(l, r, array);
+    }
+
     l++;
   }
   return array;
 }
 
-function swap(arr, l, r) {
-  const temp = arr[l];
-  arr[l] = arr[r];
-  arr[r] = temp;
-}
+const swap = (l, r, array) => {
+  const tmp = array[l];
+  array[l] = array[r];
+  array[r] = tmp;
+};
 
-// const array = [2, 1, 2, 2, 2, 3, 4, 2];
-// const toMove = 2;
+const array = [2, 1, 2, 2, 2, 3, 4, 2];
+const toMove = 2;
+console.log(moveElementToEnd(array, toMove));
 
 // console.log(moveElementToEnd(array, toMove));
 
