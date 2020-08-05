048d07f58bb305c94f8ae372639122fd44414d25
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-05 16:51:11 -0500

    Finish remove kth node from end js

--- javascript/remove_kth_node_from_end.js
+++ javascript/remove_kth_node_from_end.js
@@ -6,7 +6,25 @@ class LinkedList {
   }
 }
 
-function removeKthNodeFromEnd(head, k) {}
+function removeKthNodeFromEnd(head, k) {
+  let fast = head;
+  let slow = head;
+  let count = 1;
+  while (count <= k) {
+    fast = fast.next;
+    count += 1;
+  }
+  if (fast === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (fast.next !== null) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+}
 
 // Do not edit the line below.
 exports.LinkedList = LinkedList;
