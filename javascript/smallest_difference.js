fe294f268370f4c25721a5cdab03b17b0de5651c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-05 18:25:34 -0500

    Finish smallest difference js

--- javascript/smallest_difference.js
+++ javascript/smallest_difference.js
@@ -1,6 +1,32 @@
 function smallestDifference(arrayOne, arrayTwo) {
-  // Write your code here.
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+  console.log(arrayOne, arrayTwo);
+  left = 0;
+  right = 0;
+  smallArr = [arrayOne[0], arrayTwo[0]];
+  smallest = Infinity;
+  while (left < arrayOne.length && right < arrayTwo.length) {
+    console.log(left, right);
+    if (Math.abs(arrayOne[left] - arrayTwo[right]) < smallest) {
+      smallArr[0] = arrayOne[left];
+      smallArr[1] = arrayTwo[right];
+      smallest = Math.abs(arrayOne[left] - arrayTwo[right]);
+    }
+    if (arrayOne[left] < arrayTwo[right]) {
+      left++;
+    } else if (arrayOne[left] > arrayTwo[right]) {
+      right++;
+    } else if (arrayOne[left] === arrayTwo[right]) {
+      return [left, right];
+    }
+  }
+  return smallArr;
 }
+
+arrayOne = [-1, 5, 10, 20, 28, 3];
+arrayTwo = [26, 134, 135, 15, 17];
+console.log(smallestDifference(arrayOne, arrayTwo)); // [28, 26]
 
 // Do not edit the line below.
 exports.smallestDifference = smallestDifference;
