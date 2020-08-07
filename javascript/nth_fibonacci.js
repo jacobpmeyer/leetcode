e9c8ea5a7f3d69952bbe971d2cf1abb06d077141
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 16:48:05 -0500

    Finish nth fibonacci js

--- javascript/nth_fibonacci.js
+++ javascript/nth_fibonacci.js
@@ -1,4 +1,9 @@
-function getNthFib(n, memo = {}) {}
+function getNthFib(n, memo = {}) {
+  if (n === 1 || n === 2) return n - 1;
+  if (memo[n]) return memo[n];
+  memo[n] = getNthFib(n - 1) + getNthFib(n - 2);
+  return memo[n];
+}
 
 // Do not edit the line below.
 exports.getNthFib = getNthFib;
