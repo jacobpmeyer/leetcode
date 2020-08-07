552004325199c28e8a9afb66a6f21b2663e56615
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 17:14:33 -0500

    Finish product sum

--- javascript/product_sum.js
+++ javascript/product_sum.js
@@ -1,7 +1,26 @@
 // Tip: You can use the Array.isArray function to check whether an item
 // is a list or an integer.
-function productSum(array) {
-  // Write your code here.
+
+/**
+ *
+ * @param {array} array
+ * @return {number}
+ *
+ * steps:
+ * reduce the array.
+ * the cb should check to see if the element is array
+ * if array, recursively call product sum
+ * return reduced
+ *
+ */
+
+function productSum(array, d = 1) {
+  let total = 0;
+  array.forEach(el => {
+    if (Array.isArray(el)) total += d * productSum(el, d + 1);
+    else total += d * el;
+  });
+  return total;
 }
 
 // Do not edit the line below.
