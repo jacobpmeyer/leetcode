6417dd4c9ade084d6e594be75c70bd562e1087c3
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 17:39:34 -0500

    Finish binary search js

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -1,5 +1,17 @@
 function binarySearch(array, target) {
-  // Write your code here.
+  startIdx = 0;
+  endIdx = array.length - 1;
+  while (startIdx <= endIdx) {
+    const mid = Math.floor((startIdx + endIdx) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (target > array[mid]) {
+      startIdx = mid + 1;
+    } else {
+      endIdx = mid - 1;
+    }
+  }
+  return -1;
 }
 
 // Do not edit the line below.
