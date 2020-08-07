0b2baaeb13b551b7fd5df0c86d3da29b5d373aa1
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 16:38:28 -0500

    Start dfs js

---
+++ javascript/dfs.js
@@ -0,0 +1,31 @@
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+    const nodeChildren = [];
+    for (i = this.children.length - 1; i >= 0; i++) {
+      nodeChildren.push(this.children[i]);
+    }
+    while (nodeChildren.length > 0) {
+      const node = nodeChildren.pop();
+      node.depthFirstSearch(array);
+    }
+    return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
