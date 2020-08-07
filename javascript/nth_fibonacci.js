f198eb330efe7b2d563f571313cbe485cd4ec382
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 16:52:25 -0500

    Fix nth fibonacci

--- javascript/nth_fibonacci.js
+++ javascript/nth_fibonacci.js
@@ -1,7 +1,6 @@
-function getNthFib(n, memo = {}) {
-  if (n === 1 || n === 2) return n - 1;
-  if (memo[n]) return memo[n];
-  memo[n] = getNthFib(n - 1) + getNthFib(n - 2);
+function getNthFib(n, memo = { 1: 0, 2: 1 }) {
+  if (n in memo) return memo[n];
+  memo[n] = getNthFib(n - 1, memo) + getNthFib(n - 2, memo);
   return memo[n];
 }
 
