a468491840840fb22ea624adbc487b477a0371c9
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-07 16:53:09 -0500

    Start product sum js

---
+++ javascript/product_sum.js
@@ -0,0 +1,8 @@
+// Tip: You can use the Array.isArray function to check whether an item
+// is a list or an integer.
+function productSum(array) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.productSum = productSum;
