8ed06c2313be2e0fd24bff23b28da9fbc87c1207
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-08 16:40:26 -0500

    Finish three number sum js

--- javascript/three_number_sum.js
+++ javascript/three_number_sum.js
@@ -1,5 +1,23 @@
 function threeNumberSum(array, targetSum) {
-  // Write your code here.
+  const result = [];
+  array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i++) {
+    let l = i + 1;
+    let r = array.length - 1;
+    while (l < r) {
+      const potentialMatch = array[i] + array[l] + array[r];
+      if (potentialMatch === targetSum) {
+        result.push([array[i], array[l], array[r]]);
+        l++;
+        r--;
+      } else if (potentialMatch < targetSum) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+  }
+  return result;
 }
 
 // Do not edit the line below.
