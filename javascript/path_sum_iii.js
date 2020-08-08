1930bf757eca2157519c8c43d5cbfd77fa39d500
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-08 14:23:21 -0500

    Finish path sum iii js

--- javascript/path_sum_iii.js
+++ javascript/path_sum_iii.js
@@ -11,4 +11,22 @@
  * @param {number} sum
  * @return {number}
  */
-var pathSum = function (root, sum) {};
+var pathSum = function (root, sum) {
+  let count = 0;
+  let k = sum;
+  const h = {};
+  function preorder(node, currSum) {
+    if (node === null) return;
+    currSum += node.val;
+    if (currSum === k) count += 1;
+    if (!(currSum - k in h)) h[currSum - k] = 0;
+    count += h[currSum - sum];
+    if (!(currSum in h)) h[currSum] = 1;
+    else h[currSum] += 1;
+    preorder(node.right, currSum);
+    preorder(node.left, currSum);
+    h[currSum] -= 1;
+  }
+  preorder(root, 0);
+  return count;
+};
