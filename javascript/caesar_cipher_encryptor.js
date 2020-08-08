0387e54b721a461f05a640af3d045c10e22d6b75
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-08 16:15:43 -0500

    Finish caesar cipher encryptor js

--- javascript/caesar_cipher_encryptor.js
+++ javascript/caesar_cipher_encryptor.js
@@ -1,5 +1,17 @@
 function caesarCipherEncryptor(string, key) {
-  // Write your code here.
+  const s = [];
+  const newKey = key % 26;
+  for (const char of string) {
+    s.push(charMaker(char, newKey));
+  }
+  return s.join("");
+}
+
+function charMaker(char, key) {
+  const newChar = char.charCodeAt() + key;
+  return newChar <= 122
+    ? String.fromCharCode(newChar)
+    : String.fromCharCode(96 + (newChar % 122));
 }
 
 // Do not edit the line below.
