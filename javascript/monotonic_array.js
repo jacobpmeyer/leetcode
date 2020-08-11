71504889d71065462b3fa7e210d79bc872c70a99
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-11 12:05:05 -0500

    Finish monotonic array js

--- javascript/monotonic_array.js
+++ javascript/monotonic_array.js
@@ -1,5 +1,22 @@
+// Write a function that takes in an array of integers and returns a boolean
+// representing whether the array is monotinic.
+
+// An array is said to be monotonic if its elements, from left to right, are
+// entirely non-increasing or entireely non-decreasing.
+
 function isMonotonic(array) {
-  // Write your code here.
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > array[i - 1]) {
+      increasing = false;
+    } else if (array[i] < array[i - 1]) {
+      decreasing = false;
+    }
+  }
+
+  return increasing || decreasing;
 }
 
 // Do not edit the line below.
