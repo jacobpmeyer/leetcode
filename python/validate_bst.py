37135cbce43aa4c143d6eea304abceada76906a2
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-14 13:38:43 -0500

    Start validate bst

--- python/validate_bst.py
+++ python/validate_bst.py
@@ -1,19 +1,11 @@
-
-# O(n) Time | O(d) Space where d is the depth of the tree
-
+# This is an input class. Do not edit.
 class BST:
-	def __init__(self, value):
-		self.value = value
-		self.left = None
-		self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 def validateBst(tree):
-	return validateBstHelper(tree, float("-inf"), float("inf"))
-
-def validateBstHelper(tree, minVal, maxVal):
-	if tree is None:
-		return True
-	if tree.value < minVal or tree.value >= maxVal:
-		return False
-	leftTree = validateBstHelper(tree.left, minVal, tree.value)
-	return leftTree and validateBstHelper(tree.right, tree.value, maxVal)
+    # Write your code here.
+    pass
