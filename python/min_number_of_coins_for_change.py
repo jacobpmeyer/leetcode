550c3dc165fc28ff0b2c026803130be286afcf30
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 12:54:34 -0500

    Start min number of coins form change python

--- python/min_number_of_coins_for_change.py
+++ python/min_number_of_coins_for_change.py
@@ -1,9 +1,2 @@
 def minNumberOfCoinsForChange(n, denoms):
-	coinNums = [float("inf") for amount in range(n + 1)]
-	coinNums[0] = 0
-	for denom in denoms:
-		for amount in range(len(coinNums)):
-			if denom <= amount:
-				coinNums[amount] = min(coinNums[amount], coinNums[amount - denom] + 1)
-
-	return coinNums[n] if coinNums[n] != float("inf") else -1
+    
