4b8bd07199612420ac33555d0f6b58f7ad4a56a0
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 15:53:14 -0500

    Finish kadanes algorithm python

--- python/kadanes_algorithm.py
+++ python/kadanes_algorithm.py
@@ -1,1 +1,7 @@
 def kadanesAlgorithm(array):
+    maxSoFar = array[0]
+    maxEndingHere = array[0]
+    for i in range(1, len(array)):
+        maxEndingHere = max(maxEndingHere + array[i], array[i])
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
