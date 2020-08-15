0d051e54b260a2469145cfb357da0a6b651bce89
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 15:48:51 -0500

    Finish lenshtein distance python

--- python/kadanes.py
+++
@@ -1,9 +0,0 @@
-
-# O(n) Time where n is the length of the input array
-# O(1) Space because we are creating a fixed number of variables
-def kadanesAlgorithm(array):
-	largest, largestCurrent = array[0], array[0]
-	for num in array[1:]:
-		largestCurrent = max(num, largestCurrent + num)
-		largest = max(largest, largestCurrent)
-	return largest
--- python/levenshtein_distance.py
+++ python/levenshtein_distance.py
@@ -1,3 +1,19 @@
 def levenshteinDistance(str1, str2):
-    # Write your code here.
-    pass
+    smaller = str1 if len(str1) < len(str2) else str2
+    bigger = str1 if len(str1) >= len(str2) else str2
+    evens = [x for x in range(len(smaller) + 1)]
+    odds = [None for x in range(len(smaller) + 1)]
+    for i in range(1, len(bigger) + 1):
+        if i % 2 == 0:
+            current = evens
+            previous = odds
+        else:
+            current = odds
+            previous = evens
+        current[0] = i
+        for j in range(1, len(smaller) + 1):
+            if bigger[i - 1] == smaller[j - 1]:
+                current[j] = previous[j - 1]
+            else:
+                current[j] = 1 + min(current[j - 1], previous[j - 1], previous[j])
+    return evens[-1] if len(bigger) % 2 == 0 else odds[-1]
