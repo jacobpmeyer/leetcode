9b2bc054328ceb35fe51a65c4a8bca57f47d3005
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 12:26:36 -0500

    Finish max subset sum no adjacent python

--- python/max_subset_sum_no_adjacent.py
+++ python/max_subset_sum_no_adjacent.py
@@ -1,3 +1,11 @@
 def maxSubsetSumNoAdjacent(array):
-    # Write your code here.
-    pass
+    if not len(array):
+        return 0
+    if len(array) < 2:
+        return array[0]
+    maxSum = [None for el in range(len(array))]
+    maxSum[0] = array[0]
+    maxSum[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+        maxSum[i] = max(array[i] + maxSum[i - 2], maxSum[i - 1])
+    return maxSum[-1]
