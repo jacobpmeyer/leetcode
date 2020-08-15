70dd4224b45820a8f391b88fdda124944ce62e71
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 12:28:51 -0500

    Start number of ways to make change python

--- python/number_of_ways_to_make_change.py
+++ python/number_of_ways_to_make_change.py
@@ -1,15 +1,3 @@
-
-# O(nd) Time where n is n and d is the number of denominations | O(n) Space
 def numberOfWaysToMakeChange(n, denoms):
-	ways = [0 for amount in range(n + 1)]
-	ways[0] = 1
-	for denom in denoms:
-		for amount in range(1, n + 1):
-			if denom <= amount:
-				ways[amount] += ways[amount - denom]
-	return ways[n]
-
-
-# {"denoms": [1, 5, 10, 25], "n": 5} | expected => 2 | my code => 5
-
-print(numberOfWaysToMakeChange(5, [1, 5, 10, 25]))
+    # Write your code here.
+    pass
