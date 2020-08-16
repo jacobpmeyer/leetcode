f790824743fa151843a0a64e0de0e74f96f71663
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-16 18:39:08 -0500

    Finish balanced brackets python

--- python/balanced_brackets.py
+++ python/balanced_brackets.py
@@ -1,3 +1,15 @@
 def balancedBrackets(string):
-    # Write your code here.
-    pass
+    bracketStack = []
+    opening = { "(", "{", "[" }
+    closing = { ")": "(", "}": "{", "]": "[" }
+    for char in string:
+        if char in opening:
+            bracketStack.append(char)
+        elif char in closing:
+            if len(bracketStack):
+                popped = bracketStack.pop()
+                if popped != closing[char]:
+                    return False
+            else:
+                return False
+    return len(bracketStack) == 0
