84f9382afe2a84e472e266d812a12638d367a8db
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 19:31:23 -0500

    Finish river sizes python

--- python/river_sizes.py
+++ python/river_sizes.py
@@ -1,41 +1,29 @@
+"""
+matrix may not be a perfect square
+rivers are ones that are vertically and horizontally adjacent, not diagonally.
+the size of the river is the number of 1s adjacent 
+"""
 
-# O(nm) Time where n is the height of the matrix and m is the width of the matrix
-# O(nm) Space
 def riverSizes(matrix):
-	visited = [[False for el in row] for row in matrix]
-	sizes = []
-	for r in range(len(matrix)):
-		for c in range(len(matrix[0])):
-			if visited[r][c]:
-				continue
-			explore(r, c, visited, sizes, matrix)
-	return sizes
+    rivers = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                rivers.append(findRiverSize(matrix, i, j))
+    return rivers
 
-def explore(r, c, visited, sizes, matrix):
-	riverSize = 0
-	nodesToExplore = [[r, c]]
-	while len(nodesToExplore):
-		r, c = nodesToExplore.pop()
-		if visited[r][c]:
-			continue
-		visited[r][c] = True
-		if matrix[r][c] == 0:
-			continue
-		riverSize += 1
-		unexploredNodes = getUnexploredNodes(r, c, matrix, visited)
-		for node in unexploredNodes:
-			nodesToExplore.append(node)
-	if riverSize > 0:
-		sizes.append(riverSize)
-
-def getUnexploredNodes(r, c, matrix, visited):
-	unexploredNodes = []
-	if r > 0 and not visited[r - 1][c]:
-		unexploredNodes.append([r - 1, c])
-	if r < len(matrix) - 1 and not visited[r + 1][c]:
-		unexploredNodes.append([r + 1, c])
-	if c > 0 and not visited[r][c - 1]:
-		unexploredNodes.append([r, c - 1])
-	if c < len(matrix[0]) - 1 and not visited[r][c + 1]:
-		unexploredNodes.append([r, c + 1])
-	return unexploredNodes
+def findRiverSize(matrix, i, j):
+    dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    queue = [[i, j]]
+    size = 0
+    matrix[i][j] = -1
+    while len(queue):
+        curr = queue.pop()
+        size += 1
+        for direction in dirs:
+            r, c = curr[0] + direction[0], curr[1] + direction[1]
+            if r >= 0 and r < len(matrix) and c >= 0 and c < len(matrix[0]) \
+                    and matrix[r][c] == 1:
+                queue.append([r, c])
+                matrix[r][c] = -1   
+    return size
