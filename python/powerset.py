05fcb18c3ce354cd604b37c741a6443087bceb29
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-16 16:41:58 -0500

    Finish powerset python

--- python/powerset.py
+++ python/powerset.py
@@ -1,1 +1,7 @@
 def powerset(array):
+    sets = [[]]
+    for i in range(len(array)):
+        for j in range(len(sets)):
+            current = sets[j] + [array[i]]
+            sets.append(current)
+    return sets
