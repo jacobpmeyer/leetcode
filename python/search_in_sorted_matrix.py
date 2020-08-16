bb6f0764bceb6fc4d27bed4ba81f6aca86d8271a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-16 18:15:21 -0500

    Finish search sorted matrix python

--- python/search_in_sorted_matrix.py
+++ python/search_in_sorted_matrix.py
@@ -1,3 +1,10 @@
 def searchInSortedMatrix(matrix, target):
-    # Write your code here.
-    pass
+    r, c = 0, len(matrix[0]) - 1
+    while r < len(matrix) and c >= 0:
+        if matrix[r][c] > target:
+            c -= 1
+        elif matrix[r][c] < target:
+            r += 1
+        else:
+            return [r, c]
+    return [-1, -1]
