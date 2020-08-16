71d3832a6328f0cd51b762cf18b7bf1c28fc617b
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-15 19:53:54 -0500

    Start min heap construction python

--- python/min_heap_contruction.py
+++ python/min_heap_contruction.py
@@ -7,47 +7,25 @@ class MinHeap:
         self.heap = self.buildHeap(array)
 
     def buildHeap(self, array):
-        firstParentIdx = (len(array) - 2) // 2
-		for currentIdx in reversed(range(firstParentIdx + 1)):
-			self.siftDown(currentIdx, len(array) - 1, array)
-		return array
+        # Write your code here.
+        pass
 
-    def siftDown(self, currentIdx, endIdx, heap):
-    	childOneIdx = currentIdx * 2 + 1
-		while childOneIdx <= endIdx:
-			childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else - 1
-			if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
-				idxToSwap = childTwoIdx
-			else:
-				idxToSwap = childOneIdx
-			if heap[idxToSwap] < heap[currentIdx]:
-				self.swap(idxToSwap, currentIdx, heap)
-				currentIdx = idxToSwap
-				childOneIdx = currentIdx * 2 + 1
-			else:
-				return
+    def siftDown(self):
+        # Write your code here.
+        pass
 
-    def siftUp(self, currentIdx, heap):
-        parentIdx = (currentIdx - 1) // 2
-		while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
-			self.swap(currentIdx, parentIdx, heap)
-			currentIdx = parentIdx
-			parentIdx = (currentIdx - 1) // 2
+    def siftUp(self):
+        # Write your code here.
+        pass
 
-	# We are peaking at the root node
     def peek(self):
-        return self.heap[0]
+        # Write your code here.
+        pass
 
     def remove(self):
-        self.swap(0, len(self.heap) - 1, self.heap)
-		valueToRemove = self.heap.pop()
-		self.siftDown(0, len(self.heap) - 1, self.heap)
-		return valueToRemove
+        # Write your code here.
+        pass
 
     def insert(self, value):
-        self.heap.append(value)
-		idxToSift = len(self.heap) - 1
-		self.siftUp(idxToSift, self.heap)
-		
-	def swap(self, i, j, heap):
-		heap[i], heap[j] = heap[j], heap[i]
+        # Write your code here.
+        pass
