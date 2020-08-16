0782abe27c7522c9114ae81beb97bb5364200ef0
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-16 16:24:15 -0500

    Finish permutations python

--- python/permutations.py
+++ python/permutations.py
@@ -1,3 +1,16 @@
 def getPermutations(array):
-    # Write your code here.
-    pass
+    perms = []
+    permsHelper(0, array, perms)
+    return perms
+
+def permsHelper(idx, array, perms):
+    if idx == len(array) - 1:
+        perms.append(array[:])
+    else:
+        for j in range(idx, len(array)):
+            swap(array, j, idx)
+            permsHelper(idx + 1, array, perms)
+            swap(array, j, idx)
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
