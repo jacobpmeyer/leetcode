89b79981f32fe34c754513291e952dcfe8c2b70c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-16 20:06:17 -0500

    Start group anagrams python

--- python/group_anagrams.py
+++ python/group_anagrams.py
@@ -1,15 +1,3 @@
-# O(s*n*log(n)) Time | O(sn) Space where s is the length of the words array
-# and n is the length of the longest word
 def groupAnagrams(words):
-	anagrams = {}
-	for word in words:
-		currentSorted = "".join(sorted(word))
-		if currentSorted in anagrams:
-			anagrams[currentSorted].append(word)
-		else:
-			anagrams[currentSorted] = [word]
-	return list(anagrams.values())
-
-words = ["yo", "act", "flop", "tac", "cat", "oy", "olfp"]
-# [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"]]
-print(groupAnagrams(words))
+    # Write your code here.
+    pass
