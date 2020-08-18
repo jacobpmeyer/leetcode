b83dd8390aac25bd91240557bf4d1dd692df2c18
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-18 10:40:05 -0500

    Finish suffix trie construction python

--- python/suffix_trie_construction.py
+++ python/suffix_trie_construction.py
@@ -9,9 +9,23 @@ class SuffixTrie:
         self.populateSuffixTrieFrom(string)
 
     def populateSuffixTrieFrom(self, string):
-        # Write your code here.
-        pass
+        for i in range(len(string)):
+            self.instertSuffixStartingAt(i, string)
+
+    def instertSuffixStartingAt(self, i, string):
+        node = self.root
+        for j in range(i, len(string)):
+            letter = string[j]
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        node[self.endSymbol] = True
 
     def contains(self, string):
-        # Write your code here.
-        pass
+        node = self.root
+        for i in range(len(string)):
+            letter = string[i]
+            if letter not in node:
+                return False
+            node = node[letter]
+        return self.endSymbol in node
