e36bd98bfa25e802f6b65d1c65dcc3f060cc1e5a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-18 12:40:11 -0500

    Implement naive solution for subarray sort python

--- python/subarray_sort.py
+++ python/subarray_sort.py
@@ -1,3 +1,14 @@
+
+# O(nlog(n)) Time | O(n) Space
 def subarraySort(array):
-    # Write your code here.
-    pass
+    sortedArray = sorted(array)
+    left = 0
+    while left < len(array) and array[left] == sortedArray[left]:
+        left += 1
+
+    right = len(array) - 1
+
+    while right >= 0 and array[right] == sortedArray[right]:
+        right -= 1
+        
+    return [left, right] if left < right else [-1, -1]
