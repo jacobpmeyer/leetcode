b82a21c6f07fb048079747b93815566df060e550
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-18 18:34:40 -0500

    Finish longest range python

--- python/largest_range.py
+++ python/largest_range.py
@@ -1,3 +1,24 @@
 def largestRange(array):
-    # Write your code here.
-    pass
+	hash = {}
+	bestRange = []
+	longestRange = 0
+	for num in array:
+		hash[num] = True
+	for num in array:
+		if not hash[num]:
+			continue
+		currentRange = 1
+		left = num - 1
+		right = num + 1
+		while left in hash:
+			hash[left] = False
+			left -= 1
+			currentRange += 1			
+		while right in hash:
+			hash[right] = False
+			right += 1
+			currentRange += 1
+		if currentRange > longestRange:
+			longestRange = currentRange
+			bestRange = [left + 1, right - 1]
+	return bestRange
