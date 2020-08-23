87022a41f3c6645a84aded615888b9e30badf9a8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-22 19:45:24 -0500

    Finish max path sum in binary tree python

--- python/max_path_sum_in_binary_tree.py
+++ python/max_path_sum_in_binary_tree.py
@@ -1,3 +1,22 @@
 def maxPathSum(tree):
-    # Write your code here.
-    pass
+   currentMax = [float("-inf")]
+   getMaxPath(tree, currentMax)
+   return currentMax[0]
+
+def getMaxPath(node, currentMax):
+    if node is None:
+        return 0
+    
+    leftMax = getMaxPath(node.left, currentMax)
+    rightMax = getMaxPath(node.right, currentMax)
+
+    totalSubTree = leftMax + rightMax + node.value
+    leftPlusNode = leftMax + node.value
+    rightPlusNode = rightMax + node.value
+    currentMax[0] = max(currentMax[0], totalSubTree, leftPlusNode, rightPlusNode)
+
+    if leftMax > rightMax:
+        return leftMax + node.value
+    else:
+        return rightMax + node.value
+    
