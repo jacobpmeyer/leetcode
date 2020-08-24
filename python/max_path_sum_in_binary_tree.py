16dbbb764f083f99d7c4eb71211f882aab1bbea7
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-24 11:18:14 -0500

    Update max path sum solution python

--- python/max_path_sum_in_binary_tree.py
+++ python/max_path_sum_in_binary_tree.py
@@ -1,23 +1,43 @@
 # My version
+# def maxPathSum(tree):
+#    currentMax = [float("-inf")]
+#    getMaxPath(tree, currentMax)
+#    return currentMax[0]
+
+# def getMaxPath(node, currentMax):
+#     if node is None:
+#         return 0
+    
+#     leftMax = getMaxPath(node.left, currentMax)
+#     rightMax = getMaxPath(node.right, currentMax)
+
+#     totalSubTree = leftMax + rightMax + node.value
+#     leftPlusNode = leftMax + node.value
+#     rightPlusNode = rightMax + node.value
+#     currentMax[0] = max(currentMax[0], totalSubTree, leftPlusNode, rightPlusNode)
+
+#     if leftMax > rightMax:
+#         return leftMax + node.value
+#     else:
+#         return rightMax + node.value
+    
+
+#AE version
 def maxPathSum(tree):
-   currentMax = [float("-inf")]
-   getMaxPath(tree, currentMax)
-   return currentMax[0]
-
-def getMaxPath(node, currentMax):
-    if node is None:
-        return 0
+    _, maxSum = findMaxSum(tree)
+    return maxSum
     
-    leftMax = getMaxPath(node.left, currentMax)
-    rightMax = getMaxPath(node.right, currentMax)
+def findMaxSum(tree):
+    if tree is None:
+        return (0, 0)
 
-    totalSubTree = leftMax + rightMax + node.value
-    leftPlusNode = leftMax + node.value
-    rightPlusNode = rightMax + node.value
-    currentMax[0] = max(currentMax[0], totalSubTree, leftPlusNode, rightPlusNode)
+    leftMaxSumAsBranch, leftMaxPathSum = findMaxSum(tree.left)
+    rightMaxSumAsBranch, rightMAxPathSum findMaxSum(tree.right)
+    maxChildSumAsBranch = max(leftMaxSumAsBranch, rightMaxSumAsBranch)
 
-    if leftMax > rightMax:
-        return leftMax + node.value
-    else:
-        return rightMax + node.value
-    
+    value = tree.value
+    maxSumAsBranch = max(maxChildSumAsBranch + value, value)
+    maxSumAsRootNode = max(leftMaxSumAsBranch + value + rightMaxSumAsBranch, maxSumAsBranch)
+    maxPathSum = max(leftMaxPathSum, rightMAxPathSum, maxSumAsRootNode)
+
+    return (maxSumAsBranch, maxPathSum)
