57c640a63ae850cc93ed05dc3b397332c975713f
Jacob Meyer <jacobpmeyer87@gmail.com>
2022-02-21 14:20:21 -0600

    Reformat quicksort

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -1,31 +1,33 @@
 def quickSort(array):
-	quickSortHelper(array, 0, len(array) - 1)
-	return array
+    quickSortHelper(array, 0, len(array) - 1)
+    return array
+
 
 def quickSortHelper(array, startIdx, endIdx):
-	if startIdx >= endIdx:
-		return 
-	pivotIdx = startIdx
-	leftIdx = startIdx + 1
-	rightIdx = endIdx
-	while leftIdx <= rightIdx:
-		if array[leftIdx] > array[rightIdx] and array[rightIdx] < array[pivotIdx]:
-			swap(leftIdx, rightIdx, array)
-		if array[leftIdx] <= array[pivotIdx]:
-			leftIdx += 1
-		if array[rightIdx] >= array[pivotIdx]:
-			rightIdx -= 1
-	swap(rightIdx, pivotIdx, array)
-	leftIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
-	if leftIsSmaller:
-		quickSortHelper(array, startIdx, rightIdx - 1)
-		quickSortHelper(array, rightIdx + 1, endIdx)
-	else:
-		quickSortHelper(array, rightIdx + 1, endIdx)
-		quickSortHelper(array, startIdx, rightIdx - 1)
+    if startIdx >= endIdx:
+        return
+    pivotIdx = startIdx
+    leftIdx = startIdx + 1
+    rightIdx = endIdx
+    while leftIdx <= rightIdx:
+        if array[leftIdx] > array[rightIdx] and array[rightIdx] < array[pivotIdx]:
+            swap(leftIdx, rightIdx, array)
+        if array[leftIdx] <= array[pivotIdx]:
+            leftIdx += 1
+        if array[rightIdx] >= array[pivotIdx]:
+            rightIdx -= 1
+    swap(rightIdx, pivotIdx, array)
+    leftIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1)
+    if leftIsSmaller:
+        quickSortHelper(array, startIdx, rightIdx - 1)
+        quickSortHelper(array, rightIdx + 1, endIdx)
+    else:
+        quickSortHelper(array, rightIdx + 1, endIdx)
+        quickSortHelper(array, startIdx, rightIdx - 1)
+
 
 def swap(i, j, array):
-	array[i], array[j] = array[j], array[i]
+    array[i], array[j] = array[j], array[i]
 
 
 array = [8, 5, 2, 9, 5, 6, 3]
